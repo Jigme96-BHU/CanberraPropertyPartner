81e5fa423df14f96760e3f8ecc2206e9672e9dfe
Jigme Tharchen <gmetharchen96@gmail.com>
2026-06-09 15:10:10 +1000

    sharp package

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['sharp'],
+  },
   images: {
     formats: ['image/avif', 'image/webp'],
     minimumCacheTTL: 2592000,
